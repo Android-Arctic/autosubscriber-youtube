06cc10a343eb24525837234e7bac8f3ad12b1d89
Android-Arctic <98084612+Android-Arctic@users.noreply.github.com>
2022-02-22 19:19:08 +0530

    asas

--- GoViral.py
+++ GoViral.py
@@ -11,29 +11,6 @@ except KeyError:
     pass
 
 # Heroku
-if heroku == "available":
-    required_dict = {
-        "yt_pw": os.environ["youtube_password"],
-        "yt_email": os.environ["youtube_email"],
-        "yt_channel_id": os.environ["youtube_channel_id"],
-        "email_goviral": os.environ["goviral_email"],
-        "pw_goviral": os.environ["goviral_password"],
-        "yt_useragent": os.environ["youtube_useragent"]
-    }
-# config.ini
-else:
-    config_object = ConfigParser()
-    config_object.read("config.ini")
-    userinfo = config_object["USERINFO"]
-
-    required_dict = {
-        "yt_pw": userinfo["youtube_password"],
-        "yt_email": userinfo["youtube_email"],
-        "yt_channel_id": userinfo["youtube_channel_id"],
-        "email_goviral": userinfo["goviral_email"],
-        "pw_goviral": userinfo["goviral_password"],
-        "yt_useragent": userinfo["youtube_useragent"]
-    }
 
 if __name__ == "__main__":
-    sws.goviral_functions(required_dict)
+    sws.goviral_functions()
