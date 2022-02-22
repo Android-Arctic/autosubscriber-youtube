f8853a9994e37cb1d904eda7ebfaf0443e46f657
Android-Arctic <98084612+Android-Arctic@users.noreply.github.com>
2022-02-22 19:30:30 +0530

    afsf

--- GoViral.py
+++ GoViral.py
@@ -5,12 +5,10 @@ import os
 heroku = "not available"
 try:
     heroku = os.environ['HEROKU']
-    print("Running HEROKU Version")
+    print("Running HEROKU Version new account")
 except KeyError:
     print("Running Local Version")
     pass
-
-# Heroku
 
 if __name__ == "__main__":
     sws.goviral_functions()
